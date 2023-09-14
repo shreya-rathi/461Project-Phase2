795d09518921f16cdc50a6af802597bc3ff4b294
Jonah Nichols <jonah@Jonahs-MacBook-Pro.local>
2023-09-13 21:15:53 -0400

    Skeleton of unit tests for Score module

---
+++ tests/score.test.ts
@@ -0,0 +1,15 @@
+import { describe, it } from "@jest/globals";
+import { Score } from "../evaluator";
+
+
+let score = new Score();
+
+describe("Score", () =>
+{
+    it("Add score should append the metric's score to the metric_scores variable", () =>
+    {
+
+        
+    });
+
+});
