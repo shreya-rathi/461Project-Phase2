4ce6687e2b8f2300a8f901980d2ea947586fa337
ethanburmane <ethanburmane@gmail.com>
2023-09-23 17:45:09 -0400

    added error handling to bus factor, responsive maintainer, and net score

--- cli/src/metrics.ts
+++ cli/src/metrics.ts
@@ -1,8 +1,9 @@
-import { Package_Installer } from "./installer";
 import { execSync } from "child_process";
 import { Package } from './PKG';
 import { commit } from "isomorphic-git";
 import { get } from "http";
+import { logger } from "./logging/logger";
+import { time } from "console";
 
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 // This is the interface for the metrics. It requires each class that implements it to have a 
@@ -61,63 +62,180 @@ export class BusFactor implements Metric {
         const top_commiter_perc_func = 1 / (1 + Math.exp(-func_steepness * (top_commiter_perc - 0.5)));
         const top_x_commiter_perc_func = 1 / (1 + Math.exp(-func_steepness * (top_x_commiter_perc - 0.5)));
         const number_committers_func = 1 / (1 + Math.exp(-func_steepness * number_committers));
-        const bus_factor_score = (top_commiter_weight * top_commiter_perc_func) + (top_x_commiter_weight * top_x_commiter_perc_func) + (number_committers_weight * number_committers_func);
+        const bus_factor_score = (top_commiter_weight * top_commiter_perc_func) 
+        + (top_x_commiter_weight * top_x_commiter_perc_func) 
+        + (number_committers_weight * number_committers_func);
+        logger.info("bus factor score calculated", {
+            msg: "bus factor score calculated",
+            module: "BusFactor.prototype.score",
+            timestamp: new Date()
+        });
         return bus_factor_score;
     }
 
     public get_top_committer_perc(temp_dir: string): number {
         
         // switching to the correct directory
-        process.chdir(temp_dir);
+        try {
+            process.chdir(temp_dir);
+        } catch (error) {
+            logger.error("could not change directory to temp directory", { 
+                msg: "could not change directory to temp directory", 
+                module: "get_top_commiter_perc", 
+                timestamp: new Date() 
+            });
+            return 0;
+        }
 
         // retrieving the number of commits 
-        const commit_count_output = execSync(`git rev-list --count --all`, { encoding: 'utf-8' });
-        const commit_count: number = +commit_count_output;
+        let commit_count: number;
+        try {
+            const commit_count_output = execSync(`git rev-list --count --all`, { encoding: 'utf-8' });
+            commit_count = +commit_count_output;
+        } catch (error) {
+            logger.error("could not retrieve number of commits", {
+                msg: "could not retrieve number of commits", 
+                module: "get_top_commiter_perc",
+                timestamp: new Date() 
+            });
+            return 0;
+        }
 
         // retrieving the number of commits from the top committer and calculating the percentage
-        const commit_list_output = execSync('git shortlog -s -n', { encoding: 'utf-8' });
+        let commit_list_output: string;
+        try {
+            commit_list_output = execSync('git shortlog -s -n', { encoding: 'utf-8' });
+        } catch (error) {
+            logger.error("could not retrieve number of commits from top committer", {
+                msg: "could not retrieve number of commits from top committer",
+                module: "get_top_commiter_perc",
+                timestamp: new Date()
+            });
+            return 0;
+        }
         const first_num = commit_list_output.match(/\d+/);
         if (first_num === null) {
-            return 0; // error goes here
+            logger.error("could not retrieve number of commits from top committer", {
+                msg: "could not retrieve number of commits from top committer",
+                module: "get_top_commiter_perc",
+                timestamp: new Date()
+            });
+            return 0;
         }
         const top_commits = parseInt(first_num[0], 10)
         const top_committer_perc = top_commits / commit_count;
+        logger.info("top committer percentage calculated", { 
+            msg: "top committer percentage calculated", 
+            module: "get_top_commiter_perc", 
+            timestamp: new Date() 
+        });
         return top_committer_perc;
     }
 
     public get_top_x_committer_perc(temp_dir: string): number {
 
         // switching to the correct directory
-        process.chdir(temp_dir);
+        try {
+            process.chdir(temp_dir);
+        } catch (error) {
+            logger.error("could not change directory to temp directory", { 
+                msg: "could not change directory to temp directory", 
+                module: "get_top_commiter_perc", 
+                timestamp: new Date() 
+            });
+            return 0;
+        }
 
         // retreiving the number of commits
-        const commit_count_output = execSync(`git rev-list --count --all`, { encoding: 'utf-8' });
-        const commit_count: number = +commit_count_output;
-
+        let commit_count: number;
+        try {
+            const commit_count_output = execSync(`git rev-list --count --all`, { encoding: 'utf-8' });
+            commit_count = +commit_count_output;
+        } catch (error) {
+            logger.error("could not retrieve number of commits", {
+                msg: "could not retrieve number of commits",
+                module: "get_top_x_commiter_perc",
+                timestamp: new Date()
+            });
+            return 0;
+        }
         // retreiving the number of commits from the top x committers and calculating the percentage
-        const commit_list_output = execSync('git shortlog -s -n', { encoding: 'utf-8' });
+        let commit_list_output: string;
+        try {
+            commit_list_output = execSync('git shortlog -s -n', { encoding: 'utf-8' });
+        } catch (error) {
+            logger.error("could not retrieve number of commits from top x committers", {
+                msg: "could not retrieve number of commits from top x committers",
+                module: "get_top_x_commiter_perc",
+                timestamp: new Date()
+            });
+            return 0;
+        }
         const first_num = commit_list_output.match(/\d+/);
         if (first_num === null) {
-            return 0; // error goes here
+            logger.error("could not retrieve number of commits from top x committers", {
+                msg: "could not retrieve number of commits from top x committers",
+                module: "get_top_x_commiter_perc",
+                timestamp: new Date()
+            });
+            return 0; 
         }
         let x = 5;
         let top_x_commits = 0;
-        for (let i = 0; i < x; i++) {
-            top_x_commits += parseInt(first_num[i], 10);
+        try {
+            for (let i = 0; i < x; i++) {
+                top_x_commits += parseInt(first_num[i], 10);
+            }
+        } catch (error) {
+            logger.error("x was greater than the number of commits", {
+                msg: "x was greater than the number of commits",
+                module: "get_top_x_commiter_perc",
+                timestamp: new Date()
+            });
+            return 0;
         }
         const top_x_committer_perc = top_x_commits / commit_count;
+        logger.info("top x committer percentage calculated", {
+            msg: "top x committer percentage calculated",
+            module: "get_top_x_commiter_perc",
+            timestamp: new Date()
+        });
         return top_x_committer_perc;
     }
 
     public get_number_committers(temp_dir: string): number {
 
         // switching to the correct directory
-        process.chdir(temp_dir);
+        try {
+            process.chdir(temp_dir);
+        } catch (error) {
+            logger.error("could not change directory to temp directory", { 
+                msg: "could not change directory to temp directory", 
+                module: "get_top_commiter_perc", 
+                timestamp: new Date() 
+            });
+            return 0;
+        }
 
         // retrieving the number of committers
-        const output_buffer = execSync(`git log --format='%ae' | sort -u | wc -l`);
+        let output_buffer: string;
+        try {
+            output_buffer = execSync(`git log --format='%ae' | sort -u | wc -l`, { encoding: 'utf-8' });
+        } catch (error) {
+            logger.error("could not retrieve number of committers", {
+                msg: "could not retrieve number of committers",
+                module: "get_number_committers",
+                timestamp: new Date()
+            });
+            return 0;
+        }
         const committer_count_string = parseInt(output_buffer.toString(), 10);
         const committer_count: number = +committer_count_string
+        logger.info("number of committers calculated", {
+            msg: "number of committers calculated",
+            module: "get_number_committers",
+            timestamp: new Date()
+        });
         return committer_count;
     }
 }
@@ -184,20 +302,51 @@ export class ResponsiveMaintainer implements Metric {
         // calculating the responsive maintainer score
         const last_commit_func = 1 / (1 + Math.exp(-function_steepness * (last_commit - sigmoid_midpoint)));
         const commit_frequency_func = 1 / (1 + Math.exp(-function_steepness * commit_frequency));
-        const responsive_maintainer_score = (last_commit_weigth * last_commit_func) + (commit_frequency_weight * commit_frequency_func);
+        const responsive_maintainer_score = (last_commit_weigth * last_commit_func) 
+        + (commit_frequency_weight * commit_frequency_func);
+        logger.info("responsive maintainer score calculated", {
+            msg: "responsive maintainer score calculated",
+            module: "ResponsiveMaintainer.prototype.score",
+            timestamp: new Date()
+        });
         return responsive_maintainer_score;
     }
 
     public get_last_commit(temp_dir: string): number {
 
         // switching to the correct directory
-        process.chdir(temp_dir);
+        try {
+            process.chdir(temp_dir);
+        } catch (error) {
+            logger.error("could not change directory to temp directory", { 
+                msg: "could not change directory to temp directory", 
+                module: "get_top_commiter_perc", 
+                timestamp: new Date() 
+            });
+            return 0;
+        }
 
         // retrieving the date of the last commit
-        const last_commit_output = execSync('git log -1 --format=%ai', { encoding: 'utf-8' });
+        let last_commit_output: string;
+        try {
+            last_commit_output = execSync('git log -1 --format=%ai', { encoding: 'utf-8' });
+
+        } catch (error) {
+            logger.error("could not retrieve date of last commit", {
+                msg: "could not retrieve date of last commit",
+                module: "get_last_commit",
+                timestamp: new Date()
+            });
+            return 0;
+        }
         const last_commit_date = last_commit_output.match(/(\d{4})-(\d{2})-(\d{2})/);
         if (last_commit_date === null) {
-            return 0; // error goes here
+            logger.error("could not retrieve date of last commit", {
+                msg: "could not retrieve date of last commit",
+                module: "get_last_commit",
+                timestamp: new Date()
+            });
+            return 0;
         }
         const last_commit_year = parseInt(last_commit_date[1], 10);
         const last_commit_month = parseInt(last_commit_date[2], 10);
@@ -208,20 +357,49 @@ export class ResponsiveMaintainer implements Metric {
         const lastCommitDate = new Date(last_commit_year, last_commit_month - 1, last_commit_day);
         const time_difference = today.getTime() - lastCommitDate.getTime();
         const last_commit = Math.floor(time_difference / (1000 * 3600 * 24));
+        logger.info("number of days since last commit calculated", {
+            msg: "number of days since last commit calculated",
+            module: "get_last_commit",
+            timestamp: new Date()
+        });
         return last_commit;
     }
 
     public get_commit_frequency(temp_dir: string): number {
 
         // switching to the correct directory
-        process.chdir(temp_dir);
+        try {
+            process.chdir(temp_dir);
+        } catch (error) {
+            logger.error("could not change directory to temp directory", { 
+                msg: "could not change directory to temp directory", 
+                module: "get_top_commiter_perc", 
+                timestamp: new Date() 
+            });
+            return 0;
+        }
 
         // getting the number of commits in the last thirty days
         const thirty_days = new Date();
         thirty_days.setDate(thirty_days.getDate() - 30);
         const formatted_date = thirty_days.toISOString().split('T')[0];
-        const commit_frequency_output = execSync(`git rev-list --count --since="${formatted_date}" HEAD`, { encoding: 'utf-8' });
+        let commit_frequency_output: string;
+        try {
+            commit_frequency_output = execSync(`git rev-list --count --since="${formatted_date}" HEAD`, { encoding: 'utf-8' });
+        } catch (error) {
+            logger.error("could not retrieve number of commits in the last thirty days", {
+                msg: "could not retrieve number of commits in the last thirty days",
+                module: "get_commit_frequency",
+                timestamp: new Date()
+            });
+            return 0;
+        }
         const commit_frequency: number = +commit_frequency_output;
+        logger.info("number of commits in the last thirty days calculated", {
+            msg: "number of commits in the last thirty days calculated",
+            module: "get_commit_frequency",
+            timestamp: new Date()
+        });
         return commit_frequency;
     }
 }
@@ -274,10 +452,16 @@ export class NetScore implements Metric {
             "RESPONSIVE_MAINTAINER_SCORE": responsive_maintainer_score,
             "LICENSE_SCORE": license_score
         }];
+        
         const ndjson_output = score_json.map((obj) => {
             return JSON.stringify(obj);
         }).join('\n');
         process.stdout.write(ndjson_output);
+        logger.info("net score calculated", {
+            msg: "net score calculated",
+            module: "NetScore.prototype.score",
+            timestamp: new Date()
+        });
         return net_score;
     }
 }
