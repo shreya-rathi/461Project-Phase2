2579a920afd02a1dc793676d8f01ccc615bf217d
JNicho206 <89484640+JNicho206@users.noreply.github.com>
2023-09-24 13:19:10 -0400

    Update logger.ts to use log level and log path from environment

--- cli/src/logging/logger.ts
+++ cli/src/logging/logger.ts
@@ -1,5 +1,3 @@
-import { timeStamp } from "console";
-
 const pino = require("pino");
 const fs = require("fs");
 
@@ -11,39 +9,41 @@ if (!fs.existsSync(logDirectory)) {
   fs.mkdirSync(logDirectory);
 }
 
-// Define log levels and their respective file names
-const logLevels = ['info', 'warn', 'error', 'fatal'];
+const filePath = process.env.LOG_FILE;
 
-// Create separate transport streams for each log level
-const transports: any = {};
-logLevels.forEach((level) => {
-  transports[level] = pino.destination({
-    dest: `${logDirectory}/${level}.log`, // Set the file path for each log level
-    level, // Set the log level for this transport stream
-  });
-});
+if (fs.existsSync(filePath)) {
+  console.log('The file already exists.');
+} else {
+  // If the file does not exist, create it
+  fs.writeFileSync(filePath, 'Hello, world!', 'utf-8');
+  console.log('File created successfully.');
+}
 
+
+const lg_level = process.env.LOG_LEVEL;
+let l: number = parseInt(lg_level ?? "1", 10);
+let level: string = 'info';
+if (l == 0)
+{
+  level = 'silent';
+}
+else if (l == 2)
+{
+  level = 'debug';
+}
 // Create the Pino logger instance with the transports
 export const logger = pino({
-  levels: {
-    info: 30,
-    warn: 40,
-    error: 50,
-    fatal: 60
-  },
-  formatters: {
-    level: (label: string) => ({ level: label }), // Include the log level in the output
-  },
-  transports: transports,
+  level: level, // Set the default log level
+  dest: filePath 
 });
 
 //Examples
 //These function all take arguments of (message: string, addtional_data: Object{})
 
-logger.info("Here is an info log", { msg: "ex: package 'pkg' scoring complete: 'score' ", timestamp: new Date() });
+// logger.info("Here is an info log", { msg: "ex: package 'pkg' scoring complete: 'score' ", timestamp: new Date() });
 
-logger.warn("Here is a warning log", { msg: "ex: using outdated version of module", module: "x", timestamp: new Date() });
+// logger.warn("Here is a warning log", { msg: "ex: using outdated version of module", module: "x", timestamp: new Date() });
 
-logger.error("Here is a error log", { msg: "ex: api response code 500 from endpoint 'github.com' ", request: "GET github.com/example", timestamp: new Date() });
+// logger.error("Here is a error log", { msg: "ex: api response code 500 from endpoint 'github.com' ", request: "GET github.com/example", timestamp: new Date() });
 
-logger.fatal("Here is a fatal log", { msg: "ex: dependency not available", dependency: "example_dep", timestampe: new Date() });
+// logger.fatal("Here is a fatal log", { msg: "ex: dependency not available", dependency: "example_dep", timestampe: new Date() });
